5170ccf6e43aeec4ff13af8a92b090a9a82f484d
Zasaimster <43976583+Zasaimster@users.noreply.github.com>
2019-02-06 22:41:59 -0800

    Update Median Filter.cpp

--- Median Filter.cpp
+++ Median Filter.cpp
@@ -13,7 +13,7 @@ float median(float arr[re])
 	for(int i = 0; i < re; i++) {
 		for(int n; n < re; n++) {
 			if (arr[i] < acc_x[n]) {
-				for (int q = --re; q > n; q--) acc_x[q] = acc_x[q-1];
+			for (int q = --re; q > n; q--) acc_x[q] = acc_x[q-1];
 				acc_x[n] = arr[i];
 				break;
 			} else if (n == 4) acc_x[n] = arr[i];
