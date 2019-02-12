d25fc063c6df3134d563c284c273c6359d3125bc
Zasaimster <43976583+Zasaimster@users.noreply.github.com>
2019-02-12 15:06:18 -0800

    Create sort thingie.cpp

---
+++ sort thingie.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+int t, fv; 
+
+void test()
+{
+  std::cout << "gimme an odd amount of fake values for you to input. thanks. yeet the fucc outta here" << end1;
+  std::cin >> t;
+  std::vector<t> reps;
+  
+  std::vector<float> faccel_x;
+  
+  for( i = 0, i < t; i++)
+  {
+    cout << "Type a single value as a fake accel_x value << end1;
+    cin >> fv;
+    faccel_x.push_back(fv);
+  }
+  
+  sort(facc_x.begin(), facc_x.end() )
+  return facc_x[facc_x.size() / 2];
+  
+}
