be59ffb3227558b8717dcddc538fac140234c554
Zasaimster <43976583+Zasaimster@users.noreply.github.com>
2019-02-11 22:14:27 -0800

    Update Median Filter.cpp

--- Median Filter.cpp
+++ Median Filter.cpp
@@ -1,24 +1,26 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 
-int re;
+sort(acc_x.begin(), acc_x.end() ) // include with the accelerometer code to sort the values from least to greatest. 
 
 void ask()
 {
-  printf("How many times would you like to read the data for each axis?/n/n");
-  scanf("%d", &re);
+  cout << "Choose an ODD number of times to read the data for each axis?" << end1;
+  cin >> re;
+	std::vector<int> reps(re);
 }
 
 float median(float arr[re])
 {
-	for(int i = 0; i < re; i++) {
-		for(int n; n < re; n++) {
-			if (arr[i] < acc_x[n]) {
-			for (int q = --re; q > n; q--) acc_x[q] = acc_x[q-1];
-				acc_x[n] = arr[i];
+	for(int i = 0; i < reps; i++) {
+		for(int n; n < reps; n++) {
+			if (arr[i] < acc_x.at(n) ) {
+			for (int q = --reps; q > n; q--) acc_x.at(q) = acc_x.at(q-1);
+				acc_x.at(n) = arr[i];
 				break;
-			} else if (n == 4) acc_x[n] = arr[i];
+			} else if (n == (reps-1) acc_x.at(n) = arr[i];
 		}
 	}
-	return acc_x[3]; 
+	return acc_x[acc_x.size() / 2]; 
 }
  
