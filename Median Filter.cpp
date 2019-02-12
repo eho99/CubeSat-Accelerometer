b5c7e2a666f2a80f93313e3845610d672725e0b7
dangthanhan507 <45112823+dangthanhan507@users.noreply.github.com>
2019-02-12 14:53:19 -0800

    Update Median Filter.cpp

--- Median Filter.cpp
+++ Median Filter.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <stdio.h>
-
+#include <sidekiq_api.h>
 sort(acc_x.begin(), acc_x.end() ) // include with the accelerometer code to sort the values from least to greatest. 
 
+#define DELTA_TIME 30
+double accel_reading = 0, prev_accel_reading = 0;
 void ask()
 {
   cout << "Choose an ODD number of times to read the data for each axis?" << end1;
@@ -24,4 +26,13 @@ float median(float arr[re])
 	}
 	return acc_x[acc_x.size() / 2]; // produces middle value. 
 }
- 
+void integrate(double &value) 
+{
+	//integrates whatever you want the value variable to be when you put into main 
+	//make sure the angle inputted is the filtered angle and not the raw angle
+	value+= (accel_reading - prev_accel_reading) * DELTA_TIME;
+}
+int main(int argc, char *argv[])
+{
+	
+}
