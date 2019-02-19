5776836ece2ea1ee0e51094d5aa29872532fe13d
dangthanhan507 <45112823+dangthanhan507@users.noreply.github.com>
2019-02-19 13:37:05 -0800

    Update IMU_TEST.cpp

--- IMU_TEST.cpp
+++ IMU_TEST.cpp
@@ -2,68 +2,68 @@
 #include <vector>
 #include <stdio.h>
 #include <sidekiq_api.h>
-sort(acc_x.begin(), acc_x.end() ) // include with the accelerometer code to sort the values from least to greatest. 
+#include <cmath>
 //delta time should be 10 ms
 #define DELTA_TIME 10
-double accel_reading = 0, prev_accel_reading = 0;
-void ask()
+
+float median(vector<int> arr)
 {
-  cout << "Choose an ODD number of times to read the data for each axis?" << end1;
-  cin >> re;
-	std::vector<int> reps(re);
+  sort(arr.begin(), arr.end());
+  if(re % 2 == 1) return arr[re/2];
+  else (re % 2 == 0) return (arr[re/2] + arr[re/2+1]) / 2;
 }
 
-float median(float arr[re]) 
+void read_imu( uint8_t card, uint8_t reg) //reads the raw values
 {
-	for(int i = 0; i < reps; i++) {
-		for(int n; n < reps; n++) {
-			if (arr[i] < acc_x.at(n) ) {
-			for (int q = --reps; q > n; q--) acc_x.at(q) = acc_x.at(q-1);
-				acc_x.at(n) = arr[i];
-				break;
-			} else if (n == (reps-1) acc_x.at(n) = arr[i];
-		}
-	}
-	return acc_x[acc_x.size() / 2]; // produces middle value. 
+  int32_t status = 0;
+  uint8_t low_byte, high_byte;
+  int result;
+  status = skiq_read_accel_reg( card, reg, &high_byte, 1 );
+  if( status == 0) status = skiq_read_accel_reg( card, reg+1, &low_byte, 1);
+  if ( status == 0 )
+  {
+      result = (((int)high_byte)<<8) | low_byte;
+      if (result >= 0x8000) result = result - 0x10000;
+      return result;
+  }
 }
-void integrate(double &value) 
-{
-	//integrates whatever you want the value variable to be when you put into main 
-	//make sure the angle inputted is the filtered angle and not the raw angle
-	value+= (accel_reading - prev_accel_reading) * DELTA_TIME;
-}
-void read_imu( uint8_t card, uint8_t reg, int *val) //reads the raw values
-{
-    int32_t status = 0;
-    uint8_t low_byte, high_byte;
-    int result;
-    status = skiq_read_accel_reg( card, reg, &high_byte, 1 );
-    if( status == 0) status = skiq_read_accel_reg( card, reg+1, &low_byte, 1);
-    if ( status == 0 )
-    {
-        result = (((int)high_byte)<<8) | low_byte;
-        if (result >= 0x8000) result = result - 0x10000;
-        *val = result;
-    }
-    else
-    {
-        printf("Error: unable to read register 0x%x (%" PRIi32 ")\n", reg, status);
-    }
-}
-
 int main(int argc, char *argv[])
 {
 	uint_8t card = 0;
-	int acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z;
-	for(int time = 0; i <= 3600000; i+=DELTA_TIME) // 100HZ of data samples for 1 hr
+  vector<int> acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z; //raw_values
+  int median_ax, median_ay, median_az, median_gx, median_gy, median_gz;
+  int angle_ax, angle_ay, angle_az;
+  int angle_gx, angle_gy, angle_gz;
+	for(int i = 0; i < 360000; i++) // 100HZ of data samples for 1 hr
 	{
-		read_imu(card, 0x3b, &acc_x);
-		read_imu(card, 0x3d, &acc_y);
-		read_imu(card, 0x3f, &acc_z);
-		read_imu(card, 0x43, &gyro_x);
-		read_imu(card, 0x45, &gyro_y);
-		read_imu(card, 0x47, &gyro_z);
-		
-	`	usleep(DELTA_TIME);
+		acc_x.insert(read_imu(card, 0x3b));
+		acc_y.insert(read_imu(card, 0x3d));
+		acc_z.insert(read_imu(card, 0x3f));
+		gyro_x.insert(read_imu(card, 0x43));
+		gyro_y.insert(read_imu(card, 0x45));
+		gyro_z.insert(read_imu(card, 0x47));
+
+    //median filter for accel
+    median_ax = median(acc_x);
+    median_ay = median(acc_y);
+    median_az = median(acc_z);
+
+    //arctan A for accel
+    angle_ax += atan2(median_ay, median_ax);
+    angle_ay += atan2(median_az, median_ax);
+    angle_az += atan2(median_az, median_ay);
+    //integrate gyro values into angle
+    if(i != 0)
+    {
+      angle_gx += (gyro_x.at(i) - gyro_x.at(i-1)) * DELTA_TIME;
+      angle_gy += (gyro_y.at(i) - gyro_y.at(i-1)) * DELTA_TIME;
+      angle_gz += (gyro_z.at(i) - gyro_z.at(i-1)) * DELTA_TIME;
+    }
+    
+    //complimentary filter
+    
+    //output for excel data table -> graph.
+	` 
+    usleep(DELTA_TIME);
 	}
 }
