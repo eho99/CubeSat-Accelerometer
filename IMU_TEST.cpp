afc817c937e573c14c88a6fff8373d839f792d81
Zasaimster <43976583+Zasaimster@users.noreply.github.com>
2019-03-12 14:43:23 -0700

    Update IMU_TEST.cpp

--- IMU_TEST.cpp
+++ IMU_TEST.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <stdio.h>
-//#include <sidekiq_api.h>
+#include <sidekiq_api.h>
 #include <cmath>
 #include<algorithm>
-//#include <unistd.h>
+#include <unistd.h>
 using namespace std;
-//delta time should be 10 ms
+delta time should be 10 ms
 #define DELTA_TIME 10
 #define GYRO_CONST 0.98
 #define ACCEL_CONST 0.02
@@ -90,12 +90,12 @@ int main(int argc, char *argv[])
 	float finalAngle_x = 0, finalAngle_y = 0, finalAngle_z = 0;
 	for (int i = 0; i < PULL_NUMBER; i++) // 100HZ of data samples for 1 hr
 	{
-		//acc_x.push_back(read_imu(card, 0x3b));
-		//acc_y.push_back(read_imu(card, 0x3d));
-		//acc_z.push_back(read_imu(card, 0x3f));
-		//gyro_x.push_back(read_imu(card, 0x43));
-		//gyro_y.push_back(read_imu(card, 0x45));
-		//gyro_z.push_back(read_imu(card, 0x47));
+		acc_x.push_back(read_imu(card, 0x3b));
+		acc_y.push_back(read_imu(card, 0x3d));
+		acc_z.push_back(read_imu(card, 0x3f));
+		gyro_x.push_back(read_imu(card, 0x43));
+		gyro_y.push_back(read_imu(card, 0x45));
+		gyro_z.push_back(read_imu(card, 0x47));
 
 		//median filter for accel
 		median_ax = median(acc_x);
